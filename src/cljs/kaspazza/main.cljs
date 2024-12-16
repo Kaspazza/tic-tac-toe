618eb579ba05fbc4289cd5851b98b3c3493312ad
kaspazza <kaspazza@gmail.com>
2024-12-16 18:31:30 +0100

    Add reagent

--- src/cljs/kaspazza/main.cljs
+++ src/cljs/kaspazza/main.cljs
@@ -1,11 +1,32 @@
-(ns kaspazza.main)
+(ns kaspazza.main
+  (:require [reagent.dom.client :as rdc]))
+
+(defn main-component []
+  [:div "hello! world"])
+
+(defonce *root (atom nil))
+
+(defn ^:after-load render!
+  []
+  (rdc/render @*root [main-component]))
+
+(defn mount-root!
+  [el-id]
+  (try
+    (let [root (-> el-id
+                   js/document.getElementById
+                   rdc/create-root)]
+      (reset! *root root)
+      (render!)) 
+    (catch :default e (js/console.log (ex-info "Mount error" {:error e})))))
 
 (defn ^:export init []
-  (println "Hello World"))
+  (try
+    (mount-root! "root")
+    (catch :default e (js/console.log (ex-info "App init has failed" e)))))
 
+(comment
 
-(comment 
-  
   (tap> (+ 1 1))
   (+ 1 1)
   ;
